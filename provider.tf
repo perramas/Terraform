040c62ccfa06aa04f4bf735033577da3beb401d5
perna <123.perna@gmail.com>
2024-06-13 12:06:18 +0000

    terraform cloud integration

--- provider.tf
+++ provider.tf
@@ -5,19 +5,32 @@ terraform {
       version = "=3.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "Agent"
-    storage_account_name = "agentperna"
-    container_name       = "tfstate"                      
-    key                  = "prod.terraform.tfstate"        
-  }
+ 
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
   #
   # delete_vm_but_do_not_delete_storage
-
+  subscription_id = var.subscription_id 
+  client_id =  var.client_id
+  client_secret =  var.client_secret
+  tenant_id =  var.tenant_id
 }
+
+variable "client_id" {
+  type = string 
+}
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
